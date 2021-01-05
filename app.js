ae22996583c5c09a26cb2c0b84c4279b60b7415c
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-01-05 14:53:40 +0530

    JS completed , moving with local storage

--- app.js
+++ app.js
@@ -6,15 +6,23 @@ const total = document.getElementById('total');
 const movie = document.getElementById('movie');
 
 ///Total Price Comes from the movie value;
-const totalPrice = movie.value;
+let totalPrice = movie.value;
 
 //count and price
  function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    
-    count.innerText = selectedSeats.length
+    const selectedSeat = selectedSeats.length
+    count.innerText = selectedSeat;
+    total.innerText = selectedSeat *totalPrice;
     
 }
+
+ movie.addEventListener('change',(e)=>{
+
+    totalPrice = movie.value;
+    updateSelectedCount();
+
+ })
 
 container.addEventListener('click',(e)=>{
 
